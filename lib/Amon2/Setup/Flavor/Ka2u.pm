ce4f1454e4b27c7a5c578b2d2a096f9942f89965
Kazuhiro Shibuya <stevenlabs@gmail.com>
2011-05-23 14:06:24 +0900

    Initial import.

---
+++ lib/Amon2/Setup/Flavor/Ka2u.pm
@@ -0,0 +1,39 @@
+package Amon2::Setup::Flavor::Ka2u;
+use strict;
+use warnings;
+use 5.008001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Amon2::Setup::Flavor::Ka2u -
+
+=head1 SYNOPSIS
+
+  use Amon2::Setup::Flavor::Ka2u;
+
+=head1 DESCRIPTION
+
+Amon2::Setup::Flavor::Ka2u is
+
+=head1 AUTHOR
+
+Kazuhiro Shibuya E<lt>stevenlabs at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Kazuhiro Shibuya
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
